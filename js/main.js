af8adf3d9d6b62fcf1d3f3d380317fe6aea93960
theryx49 <ndouken@gmail.com>
2020-04-27 01:18:30 +0200

    Carousel completed

--- js/main.js
+++ js/main.js
@@ -9,5 +9,15 @@ $(document).ready(function () {
   });
 
   // owl-Carousel for blog
-  $(".owl-carousel").owlCarousel();
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    autoplay: false,
+    autoplayTimeout: 3000,
+    dots: false,
+    nav: true,
+    navText: [
+      $(".owl-navigation .owl-nav-prev"),
+      $(".owl-navigation .owl-nav-next"),
+    ],
+  });
 });
